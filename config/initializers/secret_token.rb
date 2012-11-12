55e90b8d860fe63e8b762dabd284798d8113a8fb
ilan <ilan.berci@gmail.com>
2012-11-12 09:39:20 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MWDeckbuilder::Application.config.secret_token = 'd8b09aae530de91831e90b728d437505cc58d862a7e3ac5e1b48df0e5ec655c6523ee46c2be9b9005149d5a78d33f5411e75b8bf5bded8a86e4e93b1d189fc43'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MWDeckbuilder::Application.config.session_store :cookie_store, key: '_MW_DeckBuilder_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MWDeckbuilder::Application.config.session_store :active_record_store
