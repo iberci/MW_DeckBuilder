b188f808bf41f59f2119b28a0d3df7ebb3dbdd78
ilan <ilan.berci@gmail.com>
2012-11-21 16:35:29 -0500

    including card levels in cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,10 +1,14 @@
 class CardsController < ApplicationController
+
+  DEFAULT_PER_PAGE = 20
   def index
-    @cards = Card.order('name')
+    @cards = Card.order('name').includes(:school_levels)
     @cards = @cards.send(:for_school, params[:for_school]) if params[:for_school]
     @cards = @cards.send(:for_type, params[:for_type]) if params[:for_type]
 
-    @cards.paginate(page:(params[:page] || 1), per_page:(params[:per_page] || 20))
+    @cards = @cards.paginate(page:(params[:page] || 1), per_page:(params[:per_page] || DEFAULT_PER_PAGE))
+
+    @cards = @cards.map{|c| c.attributes.merge(:levels => c.school_levels.map {|sl| sl.attributes})}
     
     respond_to do |format|
       format.html
