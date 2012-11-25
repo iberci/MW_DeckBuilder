7a7b2fb42d2aca24c2e9628cb1161b8a992dcc74
iberci <ilan.berci@gmail.com>
2012-11-25 11:35:02 -0500

    added pdf generation

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,46 +1,63 @@
 class DecksController < ApplicationController
    require 'tempfile'
    require 'csv'
+   require 'prawn'
+
    def index
-     tokens = params['cards']
-       
      type = params['type']
 
      file = Tempfile.open('Deck', Rails.root.join('tmp')) do |f|
-        self.send("build_#{type}_file", f, tokens)
+       card_counts = params['cards'].map {|t| {:card => Card.find(t.first), :amount => t.last}}
+       self.send("write_#{type}", f, card_counts)
 
-        f.flush
-        send_file(f.path, filename:"Deck.#{type}", type:"text/#{type}")
+       f.flush
+       send_file(f.path, filename:"Deck.#{type}", type:send_type(type))
      end
    end
 
    private 
-   def build_xml_file(f, tokens)
+   def send_type(type)
+     case(type) 
+     when 'pdf' then 'application/pdf'
+     else "text/#{type}"
+     end
+   end
+
+   def write_xml(f, card_counts)
      builder = Builder::XmlMarkup.new(target:f, indent:2)
      builder.instruct!
      builder.xml do |xml| 
         xml.cards do |cards_section|
 
-           for token in tokens
+           for cc in card_counts
               cards_section.card do |card_section| 
+                 card = cc[:card]
 
-                 card  = Card.find(token.first)
                  card_section.code(card.code)
                  card_section.name(card.name)
-                 card_section.amount(token.last)
+                 card_section.amount(cc[:amount])
               end
            end
         end
      end
    end
 
-   def build_csv_file(f, tokens)
+   def write_csv(f, card_counts)
       CSV.open(f.path, "wb") do |csv|
-         for token in tokens
-           card = Card.find(token.first)
-           csv << [card.code, token.last, card.name]
+         for cc in card_counts
+           card = cc[:card]
+           csv << [card.code, cc[:amount], card.name]
          end
       end
+   end
+
+   def write_pdf(f, card_counts)
+     pdf = Prawn::Document.new
+     for cc in card_counts
+       card = cc[:card]
+       pdf.text "#{card.code}, #{cc[:amount]}, #{card.name}"
+     end
+     pdf.render_file(f.path)
    end
 
 end
